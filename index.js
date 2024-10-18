9b0b8704d6191613ccce1b8515e03c0fbb8b0ff7
Cissy <122840280+cissyfdy@users.noreply.github.com>
2024-10-18 21:02:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const submitAction = document.getElementById('myForm');
       submitAction.addEventListener('submit',function(event){
-        const fullName = document.getElemenById('fullName').value;
-        const email = document.getElemenById('email').value;
-        const message = document.getElemenById('pesan').value;
+        const fullName = document.getElementById('fullName').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('pesan').value;
         
         event.preventDefault();
         windows.alert('Test');
