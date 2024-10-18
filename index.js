dd15804304343042cb6691ef4bdcd810c91203b9
Cissy <122840280+cissyfdy@users.noreply.github.com>
2024-10-18 21:21:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-windows.alert('HALO');
+
 const submitAction = document.getElementById('myForm');
       submitAction.addEventListener('submit',function(event){
         const fullName = document.getElementById('fullName').value;
@@ -6,5 +6,11 @@ const submitAction = document.getElementById('myForm');
         const message = document.getElementById('pesan').value;
         
         event.preventDefault();
+            if (fullName && email && message) {
+            // Perform additional validation if needed
+              window.alert('Form submitted successfully!');
+            } else {
+              window.alert('Please fill in all required fields.');
+            }
 
       });
